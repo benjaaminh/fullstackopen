731e4a72bd6e1a8614ea3d6be65565227b22474d
benja <benjamin.heikkala@abo.fi>
2023-05-03 17:56:51 +0300

    finish unicafe 1.6-1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -5,12 +5,52 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [average, setAverage] = useState(0)
+
+const handleGoodClick = () => {
+    setGood(good+1)
+    setTotal(total+1)
+    setAverage(average+1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+    setTotal(total+1)
+    setAverage(average)
+  }
+
+const handleBadClick = () => {
+    setBad(bad + 1)
+    setTotal(total+1)
+    setAverage(average-1)
+  }
+
 
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      <Button handleClick={handleGoodClick} text='good' />      
+      <Button handleClick={handleNeutralClick} text='neutral' />
+      <Button handleClick={handleBadClick} text='bad' />
+      <h1>statistics</h1>
+      <Statistics number={good} text='good'/>
+      <Statistics number={neutral} text='neutral'/>
+      <Statistics number={bad} text='bad'/>
+      <Statistics number={total} text='total'/>
+      <Statistics number={average/total} text='average'/>
+      <Statistics number={good/total*100} text='positive' symbol='%'/>
     </div>
   )
 }
+
+const Button = ({ handleClick, text }) => (  
+  
+<button onClick={handleClick}>
+      {text}  
+</button>)
+
+const Statistics = ({number, text, symbol}) => <p>{text} {number} {symbol}</p> 
+
 
 export default App
