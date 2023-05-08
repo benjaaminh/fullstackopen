6a1961020137135153c608cf27b639bb310f29d9
benja <benjamin.heikkala@abo.fi>
2023-05-08 17:30:28 +0300

    fix courseinfo part1, create courseinfo part2

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,69 @@
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+  }
+  
+  const Content = (props) => {
+    return (
+      <div>
+       <Part name={props.parts[0].name} number={props.parts[0].exercises}/>
+      <Part name={props.parts[1].name} number={props.parts[1].exercises}/>
+      <Part name={props.parts[2].name} number={props.parts[2].exercises}/>
+      </div>
+    )
+    }
+  
+    const Part = (props) => {
+      return (
+        <div>
+          <p>{props.name} {props.number}</p>
+        </div>
+      )
+      }
+  
+  
+  const Total = (props) => {
+    return (
+      <div>
+      <p>Number of exercises {props.parts[0].exercises+props.parts[1].exercises+props.parts[2].exercises}</p>
+      </div>
+    )
+    }
+
+
+const Course= ({course}) => {
+  return( <div>
+    <Header course={course.name}/>
+    <Content parts={course.parts} />
+  </div>)}
+
+export default App
