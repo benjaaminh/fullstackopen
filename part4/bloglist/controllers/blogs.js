b60d389ad99c7151d42df1400719c591f2125af8
benja <benjamin.heikkala@abo.fi>
2023-07-26 20:37:12 +0300

    finish 4.19

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -39,21 +39,4 @@ const blog = request.body
  
 
 
-/*  blogsRouter.put('/:id', (request, response, next) => {
-  const body = request.body
-
-  const blog = {
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes
-  }
-
-  Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
-    .then(updatedBlog => {
-      response.json(updatedBlog)
-    })
-    .catch(error => next(error))
-}) */
-
 module.exports= blogsRouter
