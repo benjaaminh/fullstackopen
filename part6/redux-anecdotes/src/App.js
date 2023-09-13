88d7265cba471710275da596065ab65e77c4d22a
benja <benjamin.heikkala@abo.fi>
2023-09-13 17:15:16 +0300

    finish 6.4

---
+++ part6/redux-anecdotes/src/App.js
@@ -0,0 +1,45 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { giveVote, createAnecdote } from './reducers/anecdoteReducer'
+
+const App = () => {
+  const anecdotes = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  const vote = (id) => {
+    console.log('vote', id)
+    dispatch(giveVote(id))
+  }
+
+  const addAnecdote = (event) => {
+  event.preventDefault()
+  const content =event.target.anecdote.value
+  event.target.anecdote.value=''
+  dispatch(createAnecdote(content))
+  }
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      )}
+      <h2>create new</h2>
+      <form onSubmit={addAnecdote}>
+        <div>
+          <input name="anecdote" />
+          </div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
---
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -0,0 +1,59 @@
+const anecdotesAtStart = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+const getId = () => (100000 * Math.random()).toFixed(0)
+
+const asObject = (anecdote) => {
+  return {
+    content: anecdote,
+    id: getId(),
+    votes: 0
+  }
+}
+
+const initialState = anecdotesAtStart.map(asObject)
+
+const reducer = (state = initialState, action) => {
+  switch(action.type){
+    case 'GIVE_VOTE':{
+      const id =action.payload.id
+      const anecdoteToVote = state.find(a=> a.id===id)
+      const changedAnecdote={
+        ...anecdoteToVote,
+        votes:anecdoteToVote.votes+1
+      }
+      return state.map(anecdote=>
+        anecdote.id !== id ? anecdote :changedAnecdote)
+    }
+    case 'NEW_ANECDOTE':
+      return [...state,action.payload]
+    default:
+      return state
+  }
+
+}
+export const createAnecdote = (content) => {
+  return{
+    type: 'NEW_ANECDOTE',
+    payload:{
+      content,
+      id: getId(),
+      votes:0
+    }
+  }
+}
+
+export const giveVote = (id) => {
+  return {
+    type: 'GIVE_VOTE',
+    payload: {id}
+  }
+}
+
+export default reducer
