8efbc663024caa61989373c13cd4245365cede04
benja <benjamin.heikkala@abo.fi>
2023-09-13 19:02:01 +0300

    6.9

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -3,15 +3,24 @@ import { giveVote } from '../reducers/anecdoteReducer'
 const AnecdoteList = () => {
 
 
-    const anecdotes = useSelector(state => state)
+    const anecdotes = useSelector(({filter,anecdotes})=>{
+        if (filter===''){
+            return anecdotes
+        
+        }else{
+      anecdotes = anecdotes.filter(anecdote=>
+            anecdote.content.toLowerCase().indexOf(filter.toLowerCase())!==-1)
+    
+            return anecdotes
+      }
+    })
     const dispatch = useDispatch()
   
     const vote = (id) => {
         console.log('vote', id)
         dispatch(giveVote(id))
       }
-
-      
+  
     
     const sortedAnecdotes = anecdotes.sort((a, b) => b.votes - a.votes)//sorted anecdotes by votes
 
