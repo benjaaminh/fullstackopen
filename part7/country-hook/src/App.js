184d41fc02da33a2bf7705aef59a192edee71d0b
benja <benjamin.heikkala@abo.fi>
2023-10-06 16:53:17 +0300

    initialize 7.7 (not finished)

---
+++ part7/country-hook/src/App.js
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange
+  }
+}
+
+const useCountry = (name) => {
+  const [country, setCountry] = useState(null)
+
+  useEffect(() => {
+
+    axios
+      .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${name}`)
+      .then(response => {
+        setCountry(response.data)
+      })
+  }, [name])
+
+  return country
+}
+
+const Country = ({ country }) => {
+  if (!country) {
+    return null
+  }
+
+  if (!country.found) {
+    return (
+      <div>
+        not found...
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h3>{country.data.name} </h3>
+      <div>capital {country.data.capital} </div>
+      <div>population {country.data.population}</div> 
+      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
+    </div>
+  )
+}
+
+const App = () => {
+  const nameInput = useField('text')
+  const [name, setName] = useState('')
+  const country = useCountry(name)
+
+  const fetch = (e) => {
+    e.preventDefault()
+    setName(nameInput.value)
+  }
+
+  return (
+    <div>
+      <form onSubmit={fetch}>
+        <input {...nameInput} />
+        <button>find</button>
+      </form>
+
+      <Country country={country} />
+    </div>
+  )
+}
+
+export default App
