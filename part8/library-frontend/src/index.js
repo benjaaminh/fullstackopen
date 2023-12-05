0188293a454310987216b3a3083506cd67c748c1
benja <benjamin.heikkala@abo.fi>
2023-12-05 12:08:15 +0200

    initial commit 8b

---
+++ part8/library-frontend/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { ApolloClient, ApolloProvider, InMemoryCache, gql } from '@apollo/client'
+
+const client = new ApolloClient({
+    uri: 'http://localhost:4000', //remember to run server!!, node index.js in abolloserver directory
+    cache: new InMemoryCache(),
+  })
+  const query = gql`
+  query {
+    allAuthors  {
+      name,
+      born,
+      bookCount,
+      id
+    }
+  }
+`
+client.query({ query })
+  .then((response) => {
+    console.log(response.data)
+  })
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+<ApolloProvider client={client}>
+<App />
+</ApolloProvider>)
